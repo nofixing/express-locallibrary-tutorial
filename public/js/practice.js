15be15fa7d3ea1eeb1cb261fe3ae3343a962dd01
nofixing <nofixing@gmail.com>
2018-09-15 08:28:16 +0900

    111

--- public/js/practice.js
+++ public/js/practice.js
@@ -15,23 +15,20 @@ $(function(){
         }
         prevScrollpos = currentScrollPos;
         if ($(this).scrollTop() > 50) {
-            $('#back-to-top').fadeIn();
-            setTimeout(function(){ $('#back-to-top').fadeOut(); }, 2000);
+            $('#back-to-top').addClass('show');
+            setTimeout(function(){ $('#back-to-top').removeClass('show'); }, 3000);
         } else {
-            $('#back-to-top').fadeOut();
+            $('#back-to-top').removeClass('show');
         }
     };
 
     // scroll body to 0px on click
     $('#back-to-top').click(function () {
-        $('#back-to-top').tooltip('hide');
         $('body,html').animate({
             scrollTop: 0
         }, 800);
         return false;
     });
-    
-    $('#back-to-top').tooltip('show');
 
     var body = document.body,
     html = document.documentElement;
