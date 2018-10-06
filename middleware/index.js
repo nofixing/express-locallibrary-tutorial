9cecad92a711f150763ae6088061cfda06c5b429
nofixing <nofixing@gmail.com>
2018-10-07 01:43:53 +0900

    222

--- middleware/index.js
+++ middleware/index.js
@@ -8,6 +8,11 @@ function requiresLogin(req, res, next) {
   if (req.session && req.session.userId) {
     return next();
   } else {
+    if (req.query.clang == 'en') {
+      res.send("Oops!, You must be logged in to view this page. please join this site first!.");
+    } else {
+      res.send("헉. 죄송합니다. 우선 회원가입부터 하시면 됩니다.");
+    }
     res.send("You must be logged in to view this page.");
     //var err = new Error('You must be logged in to view this page.');
     //err.status = 401;
