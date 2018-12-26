b15d39bb4f0b09ec8d6b3d100c7db6ba449d233a
nofixing <nofixing@gmail.com>
2018-12-27 01:41:40 +0900

    1111

--- public/js/lyjs.js
+++ public/js/lyjs.js
@@ -10,12 +10,14 @@ $( document ).ready(function() {
       $(".nav-item:eq(2)").addClass("active");
     } else if(pah.indexOf("story_open_list") > -1) {
       $(".nav-item:eq(3)").addClass("active");
-    } else if(pah.indexOf("book/create") > -1) {
+    } else if(pah.indexOf("word_board_list") > -1) {
       $(".nav-item:eq(4)").addClass("active");
-    } else if(pah.indexOf("story/create") > -1) {
+    } else if(pah.indexOf("book/create") > -1) {
       $(".nav-item:eq(5)").addClass("active");
-    } else if(pah.indexOf("genre/create") > -1) {
+    } else if(pah.indexOf("story/create") > -1) {
       $(".nav-item:eq(6)").addClass("active");
+    } else if(pah.indexOf("genre/create") > -1) {
+      $(".nav-item:eq(7)").addClass("active");
     }
   }
     
