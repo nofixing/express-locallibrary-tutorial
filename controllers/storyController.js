76916e38626da1ef19d565d47a0d7bc3f1f01e08
nofixing <nofixing@gmail.com>
2019-06-24 20:13:07 +0900

    11111

--- controllers/storyController.js
+++ controllers/storyController.js
@@ -175,7 +175,7 @@ exports.story_detail = function(req, res, next) {
                 .exec(callback);
             }
             */
-           Word.find({story: req.params.id}).collation({locale: 'en' }).sort({create_date: -1, order: 1})
+           Word.find({story: req.params.id}).collation({locale: 'en' }).sort({create_date: 1, order: 1})
                 .exec(callback);
         },
         memo: function(callback) {
@@ -270,12 +270,12 @@ exports.story_detail = function(req, res, next) {
         var sTag = '';
         for (let i = 0; i < results.words.length; i++) {
             if (typeof results.words[i].image_address != 'undefined' && typeof results.words[i].content != 'undefined') {
-                console.log(results.words[i].image_address);
-                console.log(results.words[i].content);
+                //console.log(results.words[i].image_address);
+                //console.log(results.words[i].content);
                 sTag = "<img src="+results.words[i].image_address+"><h5>"+results.words[i].content+"</h5>";
                 highlightHtml = '<a href="#" target="_blank" class="tltp" title="'+sTag+'">$1</a>';
             } else if(typeof results.words[i].content != 'undefined') {
-                console.log('content:'+results.words[i].content);
+                //console.log('content:'+results.words[i].content);
                 sTag = "<h5>"+results.words[i].content+"</h5>";
                 highlightHtml = '<a href="#" target="_blank" class="tltp" title="'+sTag+'">$1</a>';
             } else {
