cfc054e931a4844681ff48a616825cb344556469
nofixing <nofixing@gmail.com>
2019-08-07 09:50:41 +0900

    image url

--- public/js/quill_setting.js
+++ public/js/quill_setting.js
@@ -60,10 +60,21 @@ Quill.register(LinkBlot);
 
   var quill = new Quill('#snow-container', {
     modules: {
-      toolbar: '#toolbar-container'
+        toolbar: '#toolbar-container',
+        handlers: {
+            image: imageHandler
+        }
     },
     theme: 'snow'
   });
+
+    function imageHandler() {
+        var range = this.quill.getSelection();
+        var value = prompt('What is the image URL');
+        if(value){
+            this.quill.insertEmbed(range.index, 'image', value, Quill.sources.USER);
+        }
+    }
 
   var toolbar = quill.getModule('toolbar');
   toolbar.addHandler('omega', function() {
