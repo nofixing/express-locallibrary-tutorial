9fac71c9046ff68bb2cc3227aedad7df84807d81
nofixing <nofixing@gmail.com>
2019-08-07 16:36:14 +0900

    Create file.js

---
+++ models/file.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var FileSchema = new Schema({
+    user: { type: Schema.ObjectId, ref: 'User', required: true },
+    story: { type: Schema.ObjectId, ref: 'Story', required: true },
+    file_path: {type: String},
+    file_size: {type: Number}
+});
+
+// Export model.
+module.exports = mongoose.model('File', FileSchema);
