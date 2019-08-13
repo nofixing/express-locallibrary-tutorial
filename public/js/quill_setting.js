45274b9a012913449328dd61d76aa31882adeb35
nofixing <nofixing@gmail.com>
2019-08-13 11:20:25 +0900

    Update quill_setting.js

--- public/js/quill_setting.js
+++ public/js/quill_setting.js
@@ -34,6 +34,49 @@ var toolbarOptions = [
 
 var Inline = Quill.import('blots/inline');
 
+const QuillVideo = Quill.import('formats/video');
+const BlockEmbed = Quill.import('blots/block/embed');
+
+const VIDEO_ATTRIBUTES = ['height', 'width'];
+
+// provides a custom div wrapper around the default Video blot
+class Video extends BlockEmbed {
+  static create (value) {
+    const iframeNode = QuillVideo.create(value);
+    const node = super.create();
+    node.appendChild(iframeNode);
+    return node;
+  }
+
+  static formats (domNode) {
+    const iframe = domNode.getElementsByTagName('iframe')[0];
+    return VIDEO_ATTRIBUTES.reduce(function (formats, attribute) {
+      if (iframe.hasAttribute(attribute)) {
+        formats[attribute] = iframe.getAttribute(attribute);
+      }
+      return formats;
+    }, {});
+  }
+
+  static value (domNode) {
+    return domNode.getElementsByTagName('iframe')[0].getAttribute('src');
+  }
+
+  format (name, value) {
+    if (VIDEO_ATTRIBUTES.indexOf(name) > -1) {
+      if (value) { this.domNode.setAttribute(name, value) }
+      else { this.domNode.removeAttribute(name) }
+    }
+    else { super.format(name, value) }
+  }
+}
+
+Video.blotName = 'video';
+Video.className = 'embed-container';
+Video.tagName = 'DIV';
+
+Quill.register(Video, true);
+
 class LinkBlot extends Inline {
   static create(url) {
     var node = super.create();
