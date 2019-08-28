ebaab88171eb2fa5deb25806841199dfb86f22b6
nofixing <nofixing@gmail.com>
2019-08-28 13:54:19 +0900

    Update index.js

--- middleware/index.js
+++ middleware/index.js
@@ -8,11 +8,17 @@ function requiresLogin(req, res, next) {
   if (req.session && req.session.userId) {
     return next();
   } else {
+    /*
     if (req.query.clang == 'en') {
       res.send("Oops!, You must be logged in to view this page. please join this site first!.");
     } else {
       res.send("헉. 죄송합니다. 우선 회원가입부터 하시면 됩니다.");
     }
+    */
+    if (req.session) {  
+      req.session.redirectUrl = req.headers.referer || req.originalUrl || req.url; 
+    }
+    return res.redirect('/user/login');
     //var err = new Error('You must be logged in to view this page.');
     //err.status = 401;
     //return res.redirect('/');
