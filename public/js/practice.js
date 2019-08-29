0f2da694e4735a050bf1781e5c6a02ad7165342c
nofixing <nofixing@gmail.com>
2019-08-29 14:30:20 +0900

    Update practice.js

--- public/js/practice.js
+++ public/js/practice.js
@@ -75,6 +75,8 @@ $(function(){
             data.skill = '1';
             data.importance = '1';
             data.content = '';
+            data.result = $('#SAVED').val();
+            data.fail = $('#smm').val();
             var httpType = 'https://';
             if ( $('#hostname').val().indexOf('localhost') > -1 ) httpType = 'http://';
             $.ajax({
@@ -85,33 +87,34 @@ $(function(){
                 async: false,
                 success : function(data) {
                     console.log("jb_content dblclick");
-                    selectContent = data.content;
-                    var markup = "<tr><td style='text-align: center;'><div class='checkbox'><input type='checkbox' class='wList' value='"+data.word_id+"'></div></td>";
-                    markup += "<td><input type='text' size='15' maxlength='30' class='form-control ipt' value='"+data.title+"'>";
-                    markup += "<input type='hidden' class='form-control iph' value=''>";
+                    if (data.result != data.fail) {
+                        selectContent = data.content;
+                        var markup = "<tr><td style='text-align: center;'><div class='checkbox'><input type='checkbox' class='wList' value='"+data.word_id+"'></div></td>";
+                        markup += "<td><input type='text' size='15' maxlength='30' class='form-control ipt' value='"+data.title+"'>";
+                        markup += "<input type='hidden' class='form-control iph' value=''>";
 
-                    markup += "<select class='importance form-control' style='margin-top: 10px;'>";
-                    markup += "<option value='1'>"+$('#NotImportance').val()+"</option>";
-                    markup += "<option value='2'>"+$('#Important').val()+"</option>";
-                    markup += "<option value='3'>"+$('#VeryImportance').val()+"</option>";
-                    markup += "<option value='4'>"+$('#Indispensable').val()+"</option></td>";
+                        markup += "<select class='importance form-control' style='margin-top: 10px;'>";
+                        markup += "<option value='1'>"+$('#NotImportance').val()+"</option>";
+                        markup += "<option value='2'>"+$('#Important').val()+"</option>";
+                        markup += "<option value='3'>"+$('#VeryImportance').val()+"</option>";
+                        markup += "<option value='4'>"+$('#Indispensable').val()+"</option></td>";
 
-                    markup += "<td><textarea class='form-control txt' rows='1' cols='45'>"+data.content+"</textarea>";
+                        markup += "<td><textarea class='form-control txt' rows='1' cols='45'>"+data.content+"</textarea>";
 
-                    markup += "<select class='skill form-control' style='display: inline; width: 200px; margin-top: 10px;'>";
-                    markup += "<option value='1'>"+$('#NotKnow').val()+"</option>";
-                    markup += "<option value='2'>"+$('#SawSeveralTime').val()+"</option>";
-                    markup += "<option value='3'>"+$('#BeUsedTo').val()+"</option>";
-                    markup += "<option value='4'>"+$('#RememberComplete').val()+"</option>";
-                    markup += "</select>";
-                    markup += "<button type='button' class='btn btn-primary' style='display: inline; float: right; margin-top: 8px;' onclick='imgAddress(-1)'>";
-                    markup += $('#ImageAddress').val();
-                    markup += "</button></td></tr>";
+                        markup += "<select class='skill form-control' style='display: inline; width: 200px; margin-top: 10px;'>";
+                        markup += "<option value='1'>"+$('#NotKnow').val()+"</option>";
+                        markup += "<option value='2'>"+$('#SawSeveralTime').val()+"</option>";
+                        markup += "<option value='3'>"+$('#BeUsedTo').val()+"</option>";
+                        markup += "<option value='4'>"+$('#RememberComplete').val()+"</option>";
+                        markup += "</select>";
+                        markup += "<button type='button' class='btn btn-primary' style='display: inline; float: right; margin-top: 8px;' onclick='imgAddress(-1)'>";
+                        markup += $('#ImageAddress').val();
+                        markup += "</button></td></tr>";
 
-                    $(".wtd").append(markup);
+                        $(".wtd").append(markup);
 
-                    wordList();
-        
+                        wordList();
+                    }
                 }
             });
         }
@@ -138,6 +141,8 @@ $(function(){
             data.word_id = '';
             data.skill = '1';
             data.importance = '1';
+            data.result = $('#SAVED').val();
+            data.fail = $('#smm').val();
             var httpType = 'https://';
             if ( $('#hostname').val().indexOf('localhost') > -1 ) httpType = 'http://';
             $.ajax({
@@ -147,10 +152,10 @@ $(function(){
                 url: httpType+$('#hostname').val()+'/catalog/word/create',
                 async: false,
                 success : function(data) {
-        
-                    var markup = "<li style='font-weight:bold'>"+data.title+"</li>";
-                    $(".wsd").append(markup);
-        
+                    if (data.result != data.fail) {
+                        var markup = "<li style='font-weight:bold'>"+data.title+"</li>";
+                        $(".wsd").append(markup);
+                    }
                 }
             });
         }
