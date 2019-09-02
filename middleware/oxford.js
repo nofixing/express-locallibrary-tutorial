7923d49e880d55c4c66dcf500ca6f0f794e99f78
nofixing <nofixing@gmail.com>
2019-09-02 15:55:00 +0900

    Update oxford.js

--- middleware/oxford.js
+++ middleware/oxford.js
@@ -1,68 +1,130 @@
 var https = require('https');
 
-var OxfordDictionary = function(app_id,app_key) {
-  console.log('new OxfordDictionary');
-        this.config = {
-            app_id : app_id,
-            app_key : app_key
-        };
-};    
- 
-  
-    OxfordDictionary.prototype.find = function(word,cb){
-      console.log('OxfordDictionary find start');  
-      var options = {
-            host :  'od-api.oxforddictionaries.com',
-            port : 443,
-            path : '/api/v2/entries/en-gb/'+word,
-            method : 'GET',
-            headers : {
-                    "Accept": "application/json",
-                    "app_id": this.config.app_id,
-                    "app_key": this.config.app_key
-                   }
-            };
-        /*
-        const req = https.request(options, res => {
-          console.log(`statusCode: ${res.statusCode}`);
+var OxfordDictionary = function(obj) {
+    this.config = {
+        app_id : obj.app_id,
+        app_key : obj.app_key,
+        source_lang: obj.source_lang || 'en'
+    };
+};
+
+OxfordDictionary.prototype.find = function(props) {
+    var path = validate('/api/v2/entries/', props, this);
+    var options = new OptionObj(path, this.config.app_id, this.config.app_key);
+    return buildRequest(options);
+} // .find
+
+OxfordDictionary.prototype.lemmas = function(props) {
+    var path = validate('/api/v2/lemmas/', props, this);
+    var options = new OptionObj(path, this.config.app_id, this.config.app_key);
+    return buildRequest(options);
+} // .lemmas
+
+OxfordDictionary.prototype.thesaurus = function(props) {
+    var path = validate('/api/v2/thesaurus/', props, this);
+    var options = new OptionObj(path, this.config.app_id, this.config.app_key);
+    return buildRequest(options);
+} // .thesaurus
+
+OxfordDictionary.prototype.sentences = function(props) {
+    var path = validate('/api/v2/sentences/', props, this);
+    var options = new OptionObj(path, this.config.app_id, this.config.app_key);
+    return buildRequest(options);
+} // .sentences
+
+
+// Validation function
+var validate = function(path, props, $this, dtype) {
         
-          res.on('data', d => {
-            cb(null,JSON.parse(d));
-          });
-        });
+        if ( !($this.config.app_id) || !($this.config.app_key) ) {
+            throw Error('API_ID or API_KEY is undefined or NULL.');
+        }        
+
+        if (typeof props != 'object' && typeof props != 'string') {
+            throw Error('Argument is not of proper type');
+        }
+
+        if (typeof props != 'undefined' && typeof props === 'object') {
+
+            if ( props.hasOwnProperty('word') && (typeof props.word === 'string') ) {
+                path += $this.config.source_lang + '/' + props.word.toLowerCase();
+            } else {
+                throw Error('Word argument not found');
+            }
+
+            if (props.hasOwnProperty('fields') && (typeof props.fields === 'string') ) {
+                if (path.indexOf('?') > -1) {
+                    path += '&' + props.fields.toString();
+                } else {
+                    path += '?' + props.fields.toString();
+                }
+            }
+
+            if (props.hasOwnProperty('filters') && (typeof props.filters === 'string') ) {
+                if (path.indexOf('?') > -1) {
+                    path += '&' + props.filters.toString();
+                } else {
+                    path += '?' + props.filters.toString();
+                }
+            }
+        }
         
-        req.on('error', error => {
-          console.error(error);
-          cb(error);
-        });
-        
-        req.end();
-        */
-        
+        if (typeof props === 'string' && props.length > 0) {
+            path += $this.config.source_lang + '/' + props.toLowerCase();
+        }
+
+        return path;
+}; // end validateProp
+
+// HTTPS Request Promise Builder
+var buildRequest = function(options) {
+    return new Promise(function(resolve, reject) {
         https.get(options, function(res) {
-        console.log("\nstatus code: ", res.statusCode);
-        if(res.statusCode == 404){
-            cb("No such entry found");
-        }else{
-        var data = "";
-        res.on('data', function(chunk) {
-             data +=  chunk;
-        })
-        .on('end',function(){
-            try {
-              console.log('result data:'+data);
-            //result = JSON.parse(data);
-            result = data;
-          } catch (exp) {
-            result = {'status_code': 500, 'status_text': 'JSON Parse Failed'};
-          }
-            cb(null,result);
-        })
-        .on('error',function(err){
-             cb(err);    
-        });}
-        });
-        
-    }; 
+            if (res.statusCode == 404) {
+                return reject("No such entry found."); 
+            }
+            var data = "";
+            
+            res.on('data', function (chunk) {
+                data += chunk;
+            });
+
+            res.on('end', function() {
+                var result;
+                try {
+                    result = JSON.parse(data);
+                } catch (exp) {
+                    result = {
+                        'status_code': 500,
+                        'status_text': 'JSON Parse Failed'
+                    };
+                    reject(result);
+                }
+                resolve(result);                    
+            });
+
+            res.on('error', function (err) {
+                reject(err);
+            });                
+        }); // end https.get
+    }); // end promise
+}; // end buildRequest
+
+
+// Constructor Function for Option Objects
+function OptionObj(path, app_id, app_key) {
+    var options = {
+        host :  'od-api.oxforddictionaries.com',
+        port : 443,
+        path : path,
+        method : 'GET',
+        headers : {
+            "Accept": "application/json",
+            "app_id": app_id,
+            "app_key": app_key
+            }
+        };
+    return options;
+} // end OptionObj
 
 module.exports = OxfordDictionary;
