6bceb1138dbe986239c173f51ad997e8068ecdc8
nofixing <nofixing@gmail.com>
2019-09-08 22:38:21 +0900

    3

--- public/js/bootstrap_treeview.js
+++ public/js/bootstrap_treeview.js
@@ -661,6 +661,8 @@
 
 		var style = '.node-' + this.elementId + '{';
 
+		style += 'overflow-y: scroll;';
+
 		if (this.options.color) {
 			style += 'color:' + this.options.color + ';';
 		}
@@ -695,7 +697,7 @@
 		badge: '<span class="badge"></span>'
 	};
 
-	Tree.prototype.css = '.treeview .list-group-item{cursor:pointer}.treeview span.indent{margin-left:10px;margin-right:10px}.treeview span.icon{width:12px;margin-right:5px}.treeview .node-disabled{color:silver;cursor:not-allowed}'
+	Tree.prototype.css = '.treeview .list-group-item{cursor:pointer}.treeview span.indent{margin-left:10px;margin-right:10px}.treeview span.icon{width:12px;margin-right:5px}.treeview .node-disabled{color:silver;cursor:not-allowed}';
 
 
 	/**
