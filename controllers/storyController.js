dc69728b2488ff58b5f1b13d928b0ceba28ce63b
nofixing <nofixing@gmail.com>
2020-01-02 17:28:44 +0900

    8

--- controllers/storyController.js
+++ controllers/storyController.js
@@ -961,7 +961,8 @@ exports.story_oxford_ajax = function(req, res, next) {
         if (err) { console.log(err); return next(err); }
         if (theOxfordWord.length > 0) {
             console.log('Retrieve from local DB');
-            res.send(theOxfordWord[0].data.replace(/\"/g, '|'));
+            req.body.dic_content = theOxfordWord[0].data.replace(/\"/g, '|');
+            res.send(req.body);
         } else {
             console.log('Retrieve from Oxford');
             var config = {
@@ -1083,7 +1084,8 @@ exports.story_oxford_ajaxt = function(req, res, next) {
         if (err) { console.log(err); return next(err); }
         if (theOxfordWord.length > 0) {
             console.log('Retrieve from local DB');
-            res.send(theOxfordWord[0].data.replace(/\"/g, '|'));
+            req.body.dic_content = theOxfordWord[0].data.replace(/\"/g, '|');
+            res.send(req.body);
         } else {
             console.log('Retrieve from Oxford');
             var config = {
@@ -1120,7 +1122,8 @@ exports.story_oxford_ajaxs = function(req, res, next) {
         if (err) { console.log(err); return next(err); }
         if (theOxfordWord.length > 0) {
             console.log('Retrieve from local DB');
-            res.send(theOxfordWord[0].data.replace(/\"/g, '|'));
+            req.body.dic_content = theOxfordWord[0].data.replace(/\"/g, '|');
+            res.send(req.body);
         } else {
             console.log('Retrieve from Oxford');
             var config = {
