8f5e753a43d04abdc7c57b27164ff5c72aef9ca4
nofixing <nofixing@gmail.com>
2020-01-06 09:28:22 +0900

    1

--- public/js/quill_setting.js
+++ public/js/quill_setting.js
@@ -58,7 +58,30 @@ LinkBlot.tagName = 'a';
 
 Quill.register(LinkBlot, true);
 
+const BlockEmbed = Quill.import('blots/block/embed');
+class AudioBlot extends BlockEmbed {
+  static create(url) {
+    let node = super.create();
+    node.setAttribute('src', url);
+    node.setAttribute('controls', '');
+    return node;
+  }
+  
+  static value(node) {
+    return node.getAttribute('src');
+  }
+}
+AudioBlot.blotName = 'audio';
+AudioBlot.tagName = 'audio';
+Quill.register(AudioBlot, true);
+
 var Parchment = Quill.import("parchment");
+
+let AudioClass = new Parchment.Attributor.Class('audio', 'ql-audio', {
+  scope: Parchment.Scope.EMBED
+});
+
+Quill.register(AudioClass, true);
 
 let CustomClass = new Parchment.Attributor.Class('omega', 'ql-omega', {
   scope: Parchment.Scope.INLINE
@@ -107,7 +130,14 @@ $('#InsertVideo').click(function(){
   $('#vcls')[0].click();
 });
 
-toolbar.addHandler('image', imageHandler);
+var audioButton = document.querySelector('.ql-audio');
+audioButton.addEventListener('click', function() {
+  var range = this.quill.getSelection();
+  var value = prompt('What is the audio src URL');
+  if(value){
+      this.quill.insertEmbed(range.index, 'audio', value, Quill.sources.USER);
+  }
+});
 
 var customButton = document.querySelector('.ql-omega');
 customButton.addEventListener('click', function() {
@@ -122,6 +152,8 @@ customButton.addEventListener('click', function() {
     console.log('User cursor is not in editor');
   }
 });
+
+toolbar.addHandler('image', imageHandler);
 
 $('#InsertTooltip').click(function(){
   var sTag = "<img src="+$('#recipient-name').val()+"><h4>"+$('#message-text').val()+"</h4>";
