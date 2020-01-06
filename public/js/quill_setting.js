9afd6b570cfed5b8c42c6b48cd32e93dbf536581
nofixing <nofixing@gmail.com>
2020-01-06 13:28:31 +0900

    7

--- public/js/quill_setting.js
+++ public/js/quill_setting.js
@@ -175,7 +175,7 @@ for (let i = 0; i < videos.length; i++) {
   parent.insertBefore(embedContainer, videos[i]);
   embedContainer.appendChild(videos[i]);
 }
-
+/*
 var audios = document.querySelectorAll('.ql-audio');
 for (let i = 0; i < audios.length; i++) {
   var embedContainer = document.createElement('div');
@@ -184,7 +184,7 @@ for (let i = 0; i < audios.length; i++) {
   parent.insertBefore(embedContainer, audios[i]);
   embedContainer.appendChild(audios[i]);
 }
-
+*/
   /*
   var txtArea = document.createElement('textarea');
   txtArea.style.cssText = "width: 100%;height: 750px;margin: 0;background: rgb(255, 255, 255);box-sizing: border-box;color: rgb(0, 0, 0);"+
