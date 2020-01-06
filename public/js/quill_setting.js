5f622f1ee772a34b9e31fc7fc07f0040d9c6877f
nofixing <nofixing@gmail.com>
2020-01-06 09:43:04 +0900

    2

--- public/js/quill_setting.js
+++ public/js/quill_setting.js
@@ -132,7 +132,7 @@ $('#InsertVideo').click(function(){
 
 var audioButton = document.querySelector('.ql-audio');
 audioButton.addEventListener('click', function() {
-  var range = this.quill.getSelection();
+  var range = quill.getSelection();
   var value = prompt('What is the audio src URL');
   if(value){
       this.quill.insertEmbed(range.index, 'audio', value, Quill.sources.USER);
