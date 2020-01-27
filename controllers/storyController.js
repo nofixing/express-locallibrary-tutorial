4664b91554050ee3a1ecefd3749de266f3f73ded
nofixing <nofixing@gmail.com>
2020-01-27 14:13:39 +0900

    1

--- controllers/storyController.js
+++ controllers/storyController.js
@@ -411,6 +411,7 @@ exports.story_detail = function(req, res, next) {
         var pc = req.device.type.toUpperCase() == 'DESKTOP' ? 'DESKTOP':'';
         var vName = 'story_detail';
         if (pc == '') vName = 'story_mdtl';
+        console.log('story.genre:'+results.story.genre);
         res.render(vName, 
         { title: 'Title', story:  results.story, comments: results.comments, memo: memo, memo_id: memo_id, anchor: anchor, bookMark_id: bookMark_id, 
         word_list:results.words, hostname: req.headers.host, pc: pc, userId: req.session.userId, cfnt: req.session.cfnt, book_title: book_title, book_id: book_id,
