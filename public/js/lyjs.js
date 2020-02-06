95dfaf54812433ac5aed1c99b7c862168d973792
nofixing <nofixing@gmail.com>
2020-02-06 11:03:51 +0900

    7

--- public/js/lyjs.js
+++ public/js/lyjs.js
@@ -23,6 +23,43 @@ $( document ).ready(function() {
     
 });
 
+function onSignIn(googleUser) {
+  console.log('onSignIn start');
+  var profile = googleUser.getBasicProfile();
+  
+  if ($('#userEmail').length) {
+    console.log('userEmail id exists');
+    if ($('#userEmail').val() != profile.getEmail()) {
+
+      var frm = document.createElement("form");
+      frm.setAttribute("method", "post");
+      frm.setAttribute("action", "/catalog/login");
+      frm.setAttribute("target", "_self");
+      
+      var hiddenField = document.createElement("input"); 
+      hiddenField.setAttribute("type", "hidden");
+      hiddenField.setAttribute("name", "email");
+      hiddenField.setAttribute("value", profile.getEmail());
+      frm.appendChild(hiddenField);
+      var id_token = googleUser.getAuthResponse().id_token;
+      var hiddenField1 = document.createElement("input"); 
+      hiddenField1.setAttribute("type", "hidden");
+      hiddenField1.setAttribute("name", "password");
+      hiddenField1.setAttribute("value", id_token);
+      frm.appendChild(hiddenField1);
+      var hiddenField2 = document.createElement("input"); 
+      hiddenField2.setAttribute("type", "hidden");
+      hiddenField2.setAttribute("name", "gid_token");
+      hiddenField2.setAttribute("value", id_token);
+      frm.appendChild(hiddenField2);
+      document.body.appendChild(frm);
+
+      frm.submit();
+    }
+  }
+
+}
+
 function ginit() {
   console.log('ginit start');
   var googleAuth;
@@ -31,47 +68,6 @@ function ginit() {
     googleAuth = gapi.auth2.init({
       client_id: '829220596871-tkcc5nujoge6trq2ls28rsc0bge9cp5q.apps.googleusercontent.com'
     });
-    console.log('gapi load end');
-    if ($('#userEmail').length) {
-      console.log('userEmail id exists');
-      if (googleAuth.isSignedIn.get()) {
-        console.log('googleAuth.isSignedIn.get() true');
-        var googleUser = googleAuth.currentUser.get();
-        var id_token = googleUser.getAuthResponse().id_token;
-        var profile = googleUser.getBasicProfile();
-  
-        if ($('#userEmail').val() != profile.getEmail()) {
-  
-          var frm = document.createElement("form");
-          frm.setAttribute("method", "post");
-          frm.setAttribute("action", "/catalog/login");
-          frm.setAttribute("target", "_self");
-          
-          var hiddenField = document.createElement("input"); 
-          hiddenField.setAttribute("type", "hidden");
-          hiddenField.setAttribute("name", "email");
-          hiddenField.setAttribute("value", profile.getEmail());
-          frm.appendChild(hiddenField);
-          var hiddenField1 = document.createElement("input"); 
-          hiddenField1.setAttribute("type", "hidden");
-          hiddenField1.setAttribute("name", "password");
-          hiddenField1.setAttribute("value", id_token);
-          frm.appendChild(hiddenField1);
-          var hiddenField2 = document.createElement("input"); 
-          hiddenField2.setAttribute("type", "hidden");
-          hiddenField2.setAttribute("name", "gid_token");
-          hiddenField2.setAttribute("value", id_token);
-          frm.appendChild(hiddenField2);
-          document.body.appendChild(frm);
-  
-          frm.submit();
-        }
-      } else {
-        console.log('googleAuth.isSignedIn.get() false');
-      }
-    } else {
-      console.log('userEmail id not exists');
-    }
 
   });
 
