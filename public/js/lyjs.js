d314e6476986cd4bc1670e915cd5d807e50e59ce
nofixing <nofixing@gmail.com>
2020-02-06 10:32:04 +0900

    4

--- public/js/lyjs.js
+++ public/js/lyjs.js
@@ -29,42 +29,43 @@ function ginit() {
     googleAuth = gapi.auth2.init({
       client_id: '829220596871-tkcc5nujoge6trq2ls28rsc0bge9cp5q.apps.googleusercontent.com'
     });
-  });
 
-  if ($('#userEmail').length) {
-    if (googleAuth.isSignedIn.get()) {
-      var googleUser = googleAuth.currentUser.get();
-      var id_token = googleUser.getAuthResponse().id_token;
-      var profile = googleUser.getBasicProfile();
-
-      if ($('#userEmail').val() != profile.getEmail()) {
-
-        var frm = document.createElement("form");
-        frm.setAttribute("method", "post");
-        frm.setAttribute("action", "/catalog/login");
-        frm.setAttribute("target", "_self");
-        
-        var hiddenField = document.createElement("input"); 
-        hiddenField.setAttribute("type", "hidden");
-        hiddenField.setAttribute("name", "email");
-        hiddenField.setAttribute("value", profile.getEmail());
-        frm.appendChild(hiddenField);
-        var hiddenField1 = document.createElement("input"); 
-        hiddenField1.setAttribute("type", "hidden");
-        hiddenField1.setAttribute("name", "password");
-        hiddenField1.setAttribute("value", id_token);
-        frm.appendChild(hiddenField1);
-        var hiddenField2 = document.createElement("input"); 
-        hiddenField2.setAttribute("type", "hidden");
-        hiddenField2.setAttribute("name", "gid_token");
-        hiddenField2.setAttribute("value", id_token);
-        frm.appendChild(hiddenField2);
-        document.body.appendChild(frm);
-
-        frm.submit();
+    if ($('#userEmail').length) {
+      if (googleAuth.isSignedIn.get()) {
+        var googleUser = googleAuth.currentUser.get();
+        var id_token = googleUser.getAuthResponse().id_token;
+        var profile = googleUser.getBasicProfile();
+  
+        if ($('#userEmail').val() != profile.getEmail()) {
+  
+          var frm = document.createElement("form");
+          frm.setAttribute("method", "post");
+          frm.setAttribute("action", "/catalog/login");
+          frm.setAttribute("target", "_self");
+          
+          var hiddenField = document.createElement("input"); 
+          hiddenField.setAttribute("type", "hidden");
+          hiddenField.setAttribute("name", "email");
+          hiddenField.setAttribute("value", profile.getEmail());
+          frm.appendChild(hiddenField);
+          var hiddenField1 = document.createElement("input"); 
+          hiddenField1.setAttribute("type", "hidden");
+          hiddenField1.setAttribute("name", "password");
+          hiddenField1.setAttribute("value", id_token);
+          frm.appendChild(hiddenField1);
+          var hiddenField2 = document.createElement("input"); 
+          hiddenField2.setAttribute("type", "hidden");
+          hiddenField2.setAttribute("name", "gid_token");
+          hiddenField2.setAttribute("value", id_token);
+          frm.appendChild(hiddenField2);
+          document.body.appendChild(frm);
+  
+          frm.submit();
+        }
       }
     }
-  }
+
+  });
 
 }
 function leave() {
