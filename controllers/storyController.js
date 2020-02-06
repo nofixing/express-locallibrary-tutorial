b9d314438f4ddbd6c2039fa6ac4e9bd855dda2ed
nofixing <nofixing@gmail.com>
2020-02-07 08:39:24 +0900

    2

--- controllers/storyController.js
+++ controllers/storyController.js
@@ -341,9 +341,9 @@ exports.story_detail = function(req, res, next) {
         results.story.content = entities.decode(results.story.content);
         
         for (let i = 0; i < results.words.length; i++) {
-            if (typeof results.words[i].oxford_word != 'undefined') {
+            if (typeof results.words[i].oxford_word != 'undefined' && results.words[i].oxford_word != null) {
                 console.log('results.words[i].oxford_word:'+results.words[i].oxford_word);
-                if (results.words[i].oxford_word.word !== null) {
+                if (typeof results.words[i].oxford_word.word != 'undefined' && results.words[i].oxford_word.word != null) {
                     results.words[i].title = results.words[i].oxford_word.word;
                 }
             }
