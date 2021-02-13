4c645a646aea842d86166c20e9c795203b5ed883
nofixing <nofixing@gmail.com>
2021-02-13 17:57:25 +0900

    4

--- public/js/idx.js
+++ public/js/idx.js
@@ -1,18 +1,3 @@
-$( document ).ready(function() {
-    
-  var cfnt = $("#cfnt").val();
-  var cfwt = $("#cfwt").val();
-  if(typeof cfnt != 'undefined' && cfnt != '') {
-    if(typeof cfwt != 'undefined' && cfwt != '') {
-      $( document.body ).css({"font-family": cfnt});
-      $( document.body ).css({"font-weight": cfwt});
-    } else {
-      $( document.body ).css({"font-family": cfnt});
-    }
-  } else {
-    if(typeof cfwt != 'undefined' && cfwt != '') {
-      $( document.body ).css({"font-weight": cfwt});
-    }
-  }
-    
+$(function () {
+  
 });
