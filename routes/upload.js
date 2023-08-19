fe57fdb8fd5d4cd5f2ea596f8836efc075f006e2
nofixing <nofixing@gmail.com>
2023-08-20 02:08:58 +0900

    66

--- routes/upload.js
+++ routes/upload.js
@@ -1,21 +1,21 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var Story = require('../models/story');
-var Genre = require('../models/genre');
-var Word = require('../models/word');
-var Memo = require('../models/memo');
-var Book = require('../models/book');
-var Comment = require('../models/comment');
-var History = require('../models/history');
-var BookMark = require('../models/bookMark');
-var File = require('../models/file');
-var User = require('../models/user');
-var bcrypt = require('bcrypt');
+var Story = require("../models/story");
+var Genre = require("../models/genre");
+var Word = require("../models/word");
+var Memo = require("../models/memo");
+var Book = require("../models/book");
+var Comment = require("../models/comment");
+var History = require("../models/history");
+var BookMark = require("../models/bookMark");
+var File = require("../models/file");
+var User = require("../models/user");
+var bcrypt = require("bcrypt");
 
-const { Storage } = require('@google-cloud/storage');
-const Multer = require('multer');
-const { format } = require('util');
-const projectId = 'infinitestorlet';
+const { Storage } = require("@google-cloud/storage");
+const Multer = require("multer");
+const { format } = require("util");
+const projectId = "infinitestorlet";
 
 // const storage = new Storage({
 //   projectId: projectId,
@@ -24,49 +24,45 @@ const projectId = 'infinitestorlet';
 //     client_email: process.env.GOOGLE_CLIENT_EMAIL
 //   }
 // });
-const storage = new Storage({ keyFilename: 'google-credentials.json' });
+const storage = new Storage({ keyFilename: "google-credentials.json" });
 
 // Multer is required to process file uploads and make them available via
 // req.files.
 const multer = Multer({
   storage: Multer.memoryStorage(),
   limits: {
-    fileSize: 10 * 1024 * 1024, // no larger than 10mb, you can change as needed.
+    fileSize: 33 * 1024 * 1024, // no larger than 10mb, you can change as needed.
   },
 });
 
 const bucket = storage.bucket(projectId);
 
-router.post('/', multer.single('file_upload'), (req, res, next) => {
-  console.log('upload started');
+router.post("/", multer.single("file_upload"), (req, res, next) => {
+  console.log("upload started");
   if (!req.file) {
-    console.log('No file uploaded.');
-    res.status(400).send('No file uploaded.');
+    console.log("No file uploaded.");
+    res.status(400).send("No file uploaded.");
     return;
   }
 
   // Create a new blob in the bucket and upload the file data.
-  console.log('req.file.originalname:' + req.file.originalname);
-  const gcsFileName = `${req.session.userId}/${Date.now()}-${
-    req.file.originalname
-  }`;
+  console.log("req.file.originalname:" + req.file.originalname);
+  const gcsFileName = `${req.session.userId}/${Date.now()}-${req.file.originalname}`;
   const blob = bucket.file(gcsFileName);
   const blobStream = blob.createWriteStream();
 
-  blobStream.on('error', (err) => {
-    console.log('file upload error:' + err);
+  blobStream.on("error", (err) => {
+    console.log("file upload error:" + err);
     next(err);
   });
 
-  blobStream.on('finish', () => {
+  blobStream.on("finish", () => {
     // The public URL can be used to directly access the file via HTTP.
-    const publicUrl = format(
-      `https://storage.googleapis.com/${bucket.name}/${blob.name}`
-    );
+    const publicUrl = format(`https://storage.googleapis.com/${bucket.name}/${blob.name}`);
     console.log(publicUrl);
 
-    var arr = publicUrl.split('/');
-    var lnum = publicUrl.split('/').length - 1;
+    var arr = publicUrl.split("/");
+    var lnum = publicUrl.split("/").length - 1;
     var vsrc = arr[lnum];
 
     var file = new File({
@@ -83,10 +79,8 @@ router.post('/', multer.single('file_upload'), (req, res, next) => {
         console.log(err);
         return next(err);
       }
-      console.log('story file created');
-      res
-        .status(200)
-        .send(publicUrl + '&' + theFile._id + '&' + vsrc.substring(14));
+      console.log("story file created");
+      res.status(200).send(publicUrl + "&" + theFile._id + "&" + vsrc.substring(14));
     });
     /*
     Story.update({_id: req.body.storyId}, {
@@ -102,11 +96,11 @@ router.post('/', multer.single('file_upload'), (req, res, next) => {
   blobStream.end(req.file.buffer);
 });
 
-router.post('/delete', (req, res, next) => {
-  console.log('delete started');
+router.post("/delete", (req, res, next) => {
+  console.log("delete started");
 
   // Deletes the file from the bucket
-  bucket.file(req.session.userId + '/' + req.body.file_name).delete();
+  bucket.file(req.session.userId + "/" + req.body.file_name).delete();
 
   File.findByIdAndRemove(req.body.file_id, function deleteFile(err) {
     if (err) {
@@ -116,111 +110,97 @@ router.post('/delete', (req, res, next) => {
   });
 });
 
-router.post('/deleteFiles', (req, res, next) => {
-  console.log('deleteFiles started');
+router.post("/deleteFiles", (req, res, next) => {
+  console.log("deleteFiles started");
 
   if (req.session) {
-    console.log('req session started');
+    console.log("req session started");
 
     if (req.body.email && req.body.password) {
-      console.log('req.body.email && req.body.password');
-      User.authenticate(
-        req.body.email,
-        req.body.password,
-        function (error, user) {
-          console.log('User.authenticate started');
-          if (error || !user) {
-            console.log('deleteFiles authenticate error ' + error);
-            req.body.success = 'N';
-            res.send(req.body);
-          } else {
-            console.log('bucket deleteFiles started');
-            bucket.deleteFiles(
-              {
-                prefix: `${req.session.userId}/`,
-                force: true,
-              },
-              function (err) {
-                if (err) {
-                  console.log('files delete error ' + err);
-                  return next(err);
-                }
-                console.log('data delete started');
-                Book.find({ user: req.session.userId })
-                  .remove()
-                  .exec(function (err, data) {
-                    if (!err)
-                      console.log('Book deleted:' + JSON.stringify(data));
-                  });
-                BookMark.find({ user: req.session.userId })
-                  .remove()
-                  .exec(function (err, data) {
-                    if (!err)
-                      console.log('BookMark deleted:' + JSON.stringify(data));
-                  });
-                Comment.find({ user: req.session.userId })
-                  .remove()
-                  .exec(function (err, data) {
-                    if (!err)
-                      console.log('Comment deleted:' + JSON.stringify(data));
-                  });
-                File.find({ user: req.session.userId })
-                  .remove()
-                  .exec(function (err, data) {
-                    if (!err)
-                      console.log('File deleted:' + JSON.stringify(data));
-                  });
-                Genre.find({ user: req.session.userId })
-                  .remove()
-                  .exec(function (err, data) {
-                    if (!err)
-                      console.log('Genre deleted:' + JSON.stringify(data));
-                  });
-                History.find({ user: req.session.userId })
-                  .remove()
-                  .exec(function (err, data) {
-                    if (!err)
-                      console.log('History deleted:' + JSON.stringify(data));
-                  });
-                Memo.find({ user: req.session.userId })
-                  .remove()
-                  .exec(function (err, data) {
-                    if (!err)
-                      console.log('Memo deleted:' + JSON.stringify(data));
-                  });
-                Story.find({ user: req.session.userId })
-                  .remove()
-                  .exec(function (err, data) {
-                    if (!err)
-                      console.log('Story deleted:' + JSON.stringify(data));
-                  });
-                User.find({ email: req.session.userEmail })
-                  .remove()
-                  .exec(function (err, data) {
-                    if (!err)
-                      console.log('User deleted:' + JSON.stringify(data));
-                  });
-                Word.find({ user: req.session.userId })
-                  .remove()
-                  .exec(function (err, data) {
-                    if (!err)
-                      console.log('Word deleted:' + JSON.stringify(data));
-                  });
-
-                req.session.destroy(function (err) {
-                  if (err) {
-                    console.log('session destroy error ' + err);
-                    return next(err);
-                  } else {
-                    req.body.success = 'Y';
-                    res.send(req.body);
-                  }
-                });
+      console.log("req.body.email && req.body.password");
+      User.authenticate(req.body.email, req.body.password, function (error, user) {
+        console.log("User.authenticate started");
+        if (error || !user) {
+          console.log("deleteFiles authenticate error " + error);
+          req.body.success = "N";
+          res.send(req.body);
+        } else {
+          console.log("bucket deleteFiles started");
+          bucket.deleteFiles(
+            {
+              prefix: `${req.session.userId}/`,
+              force: true,
+            },
+            function (err) {
+              if (err) {
+                console.log("files delete error " + err);
+                return next(err);
               }
-            );
-          }
+              console.log("data delete started");
+              Book.find({ user: req.session.userId })
+                .remove()
+                .exec(function (err, data) {
+                  if (!err) console.log("Book deleted:" + JSON.stringify(data));
+                });
+              BookMark.find({ user: req.session.userId })
+                .remove()
+                .exec(function (err, data) {
+                  if (!err) console.log("BookMark deleted:" + JSON.stringify(data));
+                });
+              Comment.find({ user: req.session.userId })
+                .remove()
+                .exec(function (err, data) {
+                  if (!err) console.log("Comment deleted:" + JSON.stringify(data));
+                });
+              File.find({ user: req.session.userId })
+                .remove()
+                .exec(function (err, data) {
+                  if (!err) console.log("File deleted:" + JSON.stringify(data));
+                });
+              Genre.find({ user: req.session.userId })
+                .remove()
+                .exec(function (err, data) {
+                  if (!err) console.log("Genre deleted:" + JSON.stringify(data));
+                });
+              History.find({ user: req.session.userId })
+                .remove()
+                .exec(function (err, data) {
+                  if (!err) console.log("History deleted:" + JSON.stringify(data));
+                });
+              Memo.find({ user: req.session.userId })
+                .remove()
+                .exec(function (err, data) {
+                  if (!err) console.log("Memo deleted:" + JSON.stringify(data));
+                });
+              Story.find({ user: req.session.userId })
+                .remove()
+                .exec(function (err, data) {
+                  if (!err) console.log("Story deleted:" + JSON.stringify(data));
+                });
+              User.find({ email: req.session.userEmail })
+                .remove()
+                .exec(function (err, data) {
+                  if (!err) console.log("User deleted:" + JSON.stringify(data));
+                });
+              Word.find({ user: req.session.userId })
+                .remove()
+                .exec(function (err, data) {
+                  if (!err) console.log("Word deleted:" + JSON.stringify(data));
+                });
+
+              req.session.destroy(function (err) {
+                if (err) {
+                  console.log("session destroy error " + err);
+                  return next(err);
+                } else {
+                  req.body.success = "Y";
+                  res.send(req.body);
+                }
+              });
+            }
+          );
         }
-      );
+      });
     }
   }
 });
